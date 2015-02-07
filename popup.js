7db7b798cd556a85aa1ed5f24f16f9ab4e51c6c9
Bodo Tasche <bodo@wannawork.de>
2015-02-07 23:45:52 +0100

    added jquery to the popup

--- popup.js
+++ popup.js
@@ -1,11 +1,9 @@
 function showSelect() {
   chrome.extension.getBackgroundPage().getTickets(function(tickets) {
-    console.log(tickets);
-    el = document.querySelectorAll('body')[0];
     if (tickets[0]) {
-      el.insertAdjacentHTML('afterend', "" + tickets[0].type + "[" + tickets[0].id + "] " + tickets[0].title);
+      $("#content").append(tickets[0].type + "[" + tickets[0].id + "] " + tickets[0].title);
     } else {
-      el.insertAdjacentHTML('afterend', "open ticket first");
+      $("#content").append("open ticket first");
     }
   });
 }
