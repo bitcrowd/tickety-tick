e017dfeedbfcc23810719e65cee6217969e9dd88
Bodo Tasche <bodo@wannawork.de>
2015-02-08 11:24:22 +0100

    added some styling to the popup

--- popup/popup.js
+++ popup/popup.js
@@ -2,7 +2,18 @@ var currentTickets = [];
 
 function displayHowto() {
   $("#content").empty();
-  $("#content").append("Open a ticket first");
+  $("#content").append(
+    "<h1>Help</h1>" +
+    "<p>Open a ticket and then click this extension again.</p>" +
+    "<p>" +
+      "If you have selected more than one ticket, you can then select " +
+      "the ticket to use." +
+    "</p><p>" +
+      "And as a final step you decide if you want to have a commit message or the branch name based on the ticket." +
+    "</p><p>" +
+      "Done." +
+    "</p>"
+  );
 }
 
 function displayCopyPanel(ticket) {
@@ -11,8 +22,17 @@ function displayCopyPanel(ticket) {
   var commitMessage = "[" + ticket.id + "] " + ticket.title;
   var gitBranch = cleanTitleForGitBranch(ticket.type, ticket.id + "-" + ticket.title);
 
-  $("#content").append("<button class='to-clipboard' data-clipboard-text='" + escape(commitMessage) + "'>" + commitMessage + "</button>");
-  $("#content").append("<button class='to-clipboard' data-clipboard-text='" + escape(gitBranch)     + "'>" + gitBranch     + "</button>");
+  $("#content").append("<h1>" + commitMessage + "</h1>");
+
+  linkList = "<ul class='button-list'>"+
+             "<li><a href='#' class='to-clipboard' data-clipboard-text='" + escape(commitMessage) + "'>Commit message</a></li>" +
+             "<li><a href='#' class='to-clipboard' data-clipboard-text='" + escape(gitBranch)     + "'>Branch name</a></li>" +
+             "</ul>"
+  $("#content").append(linkList);
+
+  if (currentTickets.length > 1) {
+    $("#content").append("<p><a href='#' class='to-select'>&laquo; Zurück</a></p>");
+  }
 }
 
 function cleanTitleForGitBranch(storyType, title) {
@@ -33,18 +53,30 @@ function cleanTitleForGitBranch(storyType, title) {
 function displayTicketSelect(tickets) {
   currentTickets = tickets;
   $("#content").empty();
-  $("#content").append("Select the Ticket:");
+  $("#content").append("<h1>Select the Ticket:</h1>");
+  ticketList = "";
+  tickets.sort(function(a, b) {
+    return a.title.localeCompare(b.title);
+  }).forEach(function(ticket, index) {
+    ticketList += "<li><a href='#' class='select-ticket' data-ticket-number='" + index + "'>" + ticket.title + "</a></li>";
+  });
+  $("#content").append("<ul class='button-list'>" + ticketList + "</ul>");
 }
 
 function updateTickets(tickets) {
   if (!tickets || tickets.length == 0) {
     displayHowto();
   } else if (tickets.length > 1) {
-    displayTicketSelect(tickes);
+    displayTicketSelect(tickets);
   } else if (tickets.length == 1) {
     displayCopyPanel(tickets[0]);
   }
 }
+
+$("body").on("click", ".select-ticket", function(event) {
+  var number = $(event.target).attr("data-ticket-number");
+  displayCopyPanel(currentTickets[number]);
+});
 
 $("body").on("click", ".to-clipboard", function(event) {
   var text = unescape($(event.target).attr("data-clipboard-text"));
@@ -54,6 +86,11 @@ $("body").on("click", ".to-clipboard", function(event) {
   copyFrom.select();
   document.execCommand('copy');
   copyFrom.remove();
+  window.close();
+});
+
+$("body").on("click", ".to-select", function(event) {
+  displayTicketSelect(currentTickets);
 });
 
 function showSelect() {
