84bda1e8cb016e43588fb4cd781bc5c6ab4b7baf
Bodo Tasche <bodo@wannawork.de>
2015-02-14 09:53:36 +0100

    It’s now a chrome action to make it the same on all platforms

--- src/chrome/background.js
+++ src/chrome/background.js
@@ -1,14 +1,3 @@
-function updateTickets(tabId) {
-  chrome.pageAction.hide(tabId);
-  chrome.tabs.sendRequest(tabId, {tickets: true}, function(tickets) {
-    if (tickets == null) {
-      chrome.pageAction.hide(tabId);
-    } else {
-      chrome.pageAction.show(tabId);
-    }
-  });
-}
-
 function getTickets(callback) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendRequest(tabs[0].id, {tickets: true}, function(newTickets) {
@@ -16,18 +5,3 @@ function getTickets(callback) {
     });
   });
 }
-
-chrome.tabs.onUpdated.addListener(function(tabId, change, tab) {
-  if (change.status == "complete") {
-    updateTickets(tab.id);
-  }
-});
-
-chrome.tabs.onSelectionChanged.addListener(function(tabId, info) {
-  updateTickets(tabId);
-});
-
-// Ensure the current selected tab is set up.
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  updateTickets(tabs[0].id);
-});
