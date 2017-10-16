1f6a4acd4d5219311d38f2405ddaa28675ca8958
Philipp Tessenow <philipp@tessenow.org>
2017-10-16 13:41:16 +0200

    [#35] List github-project tickets (#63)

--- src/common/adapters/github.js
+++ src/common/adapters/github.js
@@ -24,6 +24,22 @@ const adapter = {
       return fn(null, tickets);
     }
 
+    // github project
+    if ($has('.project-columns .project-card', doc)) {
+      const openProjectCardSelector = '.project-columns .project-card[data-card-state=\'["open"]\']';
+      const projectCards = $find(openProjectCardSelector, doc);
+
+      const tickets = $map(projectCards, (_, card) => {
+        const id = JSON.parse(card.dataset.cardTitle).slice(-2, -1)[0];
+        const title = $text('a.h5', card);
+        const type = JSON.parse(card.dataset.cardLabel).includes('bug') ? 'bug' : 'feature';
+
+        return { id, title, type };
+      });
+
+      return fn(null, tickets);
+    }
+
     return fn(null, null);
   }
 };
