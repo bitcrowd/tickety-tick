df7737e5d406b73244f779e32a977814b00801bc
pmeinhardt <mnhrdt@gmail.com>
2017-10-25 00:54:50 +0200

    Upgrade to React 16 and React Router v4

--- src/common/popup/components/header.jsx
+++ src/common/popup/components/header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 
 import CopyButton from './copy-button';
 import TicketShape from '../utils/ticket-shape';
