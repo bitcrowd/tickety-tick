37a7e8015431823d2a8ba54b36baf5456df2babf
pmeinhardt <mnhrdt@gmail.com>
2019-01-08 00:58:31 +0100

    Use updated promise-based storage interface

--- src/web-extension/options/components/form.test.jsx
+++ src/web-extension/options/components/form.test.jsx
@@ -7,68 +7,73 @@ import TemplateInput from './template-input';
 import { defaults as fallbacks, helpers } from '../../../common/format';
 
 describe('form', () => {
-  function render(overrides) {
-    const store = { get: jest.fn(), set: jest.fn() };
+  function render(overrides, options = {}) {
+    const store = { get: jest.fn().mockResolvedValue({}), set: jest.fn().mockResolvedValue({}) };
     const defaults = { store };
 
     const props = { ...defaults, ...overrides };
-    const wrapper = shallow(<Form {...props} />);
-    const instance = wrapper.instance();
+    const wrapper = shallow(<Form {...props} />, options);
 
-    return { wrapper, instance, props };
+    return wrapper;
   }
 
   const inputs = wrapper => wrapper.find(TemplateInput);
   const input = (wrapper, name) => inputs(wrapper).find({ name });
   const value = (wrapper, name) => input(wrapper, name).prop('value');
 
-  const change = (wrapper, instance, name, val) => {
+  const change = (wrapper, name, val) => {
     const event = { target: { name, value: val } };
-    instance.handleChanged(event);
-    wrapper.update();
+    const field = wrapper.find(TemplateInput).find({ name });
+    field.simulate('change', event);
   };
 
   it('renders a template-input for the branch name format', () => {
-    const { wrapper, instance } = render({});
-    expect(input(wrapper, 'branch').props()).toEqual({
+    const wrapper = render({});
+    const field = input(wrapper, 'branch');
+
+    expect(field.props()).toEqual({
       label: 'Branch Name Format',
       id: 'branch-name-format',
       name: 'branch',
       value: '',
       fallback: fallbacks.branch,
       disabled: true,
-      onChange: instance.handleChanged,
+      onChange: expect.any(Function),
     });
   });
 
   it('renders a template-input for the commit message format', () => {
-    const { wrapper, instance } = render({});
-    expect(input(wrapper, 'commit').props()).toEqual({
+    const wrapper = render({});
+    const field = input(wrapper, 'commit');
+
+    expect(field.props()).toEqual({
       label: 'Commit Message Format',
       id: 'commit-message-format',
       name: 'commit',
       value: '',
       fallback: fallbacks.commit,
       disabled: true,
-      onChange: instance.handleChanged,
+      onChange: expect.any(Function),
     });
   });
 
   it('renders a template-input for the command format', () => {
-    const { wrapper, instance } = render({});
-    expect(input(wrapper, 'command').props()).toEqual({
+    const wrapper = render({});
+    const field = input(wrapper, 'command');
+
+    expect(field.props()).toEqual({
       label: 'Command Format',
       id: 'command-format',
       name: 'command',
       value: '',
       fallback: fallbacks.command,
       disabled: true,
-      onChange: instance.handleChanged,
+      onChange: expect.any(Function),
     });
   });
 
   it('renders the names of available template helpers', () => {
-    const { wrapper } = render({});
+    const wrapper = render({});
     const text = wrapper.text();
 
     Object.keys(helpers).forEach((name) => {
@@ -76,76 +81,77 @@ describe('form', () => {
     });
   });
 
-  it('loads stored templates on mount', () => {
+  it('loads stored templates on mount and updates the form inputs', async () => {
     const store = { get: jest.fn(), set: jest.fn() };
-    const data = { templates: { branch: 'a', commit: 'b', command: 'c' } };
-    store.get.mockImplementation((_, fn) => fn(data));
 
-    const { instance } = render({ store });
-    jest.spyOn(instance, 'handleLoaded');
+    let loaded;
 
-    instance.componentDidMount();
+    store.get.mockReturnValue(new Promise((resolve) => {
+      // Create a new async function that resolves the promise returned from
+      // calls to `store.get()` and returns a promise itself which we can wait
+      // on to ensure the store results have been handled.
+      loaded = async function load(data) { resolve(data); };
+    }));
 
-    expect(store.get).toHaveBeenCalledWith(null, instance.handleLoaded);
-    expect(instance.handleLoaded).toHaveBeenCalledWith(data);
-  });
+    const wrapper = render({ store });
 
-  it('updates the form inputs once templates are loaded', () => {
-    const { wrapper, instance } = render({});
-    const data = { templates: { branch: 'x', commit: 'y', command: 'z' } };
+    expect(store.get).toHaveBeenCalledWith(null);
+    expect(inputs(wrapper).every({ disabled: true })).toBe(true);
 
-    instance.handleLoaded(data);
-    wrapper.update();
+    await loaded({ templates: { branch: 'a', commit: 'b', command: 'c' } });
 
-    expect(value(wrapper, 'branch')).toBe('x');
-    expect(value(wrapper, 'commit')).toBe('y');
-    expect(value(wrapper, 'command')).toBe('z');
+    expect(value(wrapper, 'branch')).toBe('a');
+    expect(value(wrapper, 'commit')).toBe('b');
+    expect(value(wrapper, 'command')).toBe('c');
 
     expect(inputs(wrapper).every({ disabled: false })).toBe(true);
   });
 
   it('updates the form inputs on changes', () => {
-    const { wrapper, instance } = render({});
+    const wrapper = render({});
 
-    change(wrapper, instance, 'branch', 'branch++');
+    change(wrapper, 'branch', 'branch++');
     expect(value(wrapper, 'branch')).toBe('branch++');
 
-    change(wrapper, instance, 'commit', 'commit++');
+    change(wrapper, 'commit', 'commit++');
     expect(value(wrapper, 'commit')).toBe('commit++');
 
-    change(wrapper, instance, 'command', 'command++');
+    change(wrapper, 'command', 'command++');
     expect(value(wrapper, 'command')).toBe('command++');
   });
 
-  it('stores templates on submit', () => {
+  it('stores templates on submit and disables form elements while saving', async () => {
     const store = { get: jest.fn(), set: jest.fn() };
-    store.set.mockImplementation((_, fn) => fn());
 
-    const { wrapper, instance } = render({ store });
-    jest.spyOn(instance, 'handleSaved').mockReturnValue();
+    let saved;
 
-    change(wrapper, instance, 'branch', 'branch++');
-    change(wrapper, instance, 'commit', 'commit++');
-    change(wrapper, instance, 'command', 'command++');
+    const unchanged = { branch: 'branch', commit: 'commit', command: 'command' };
+    store.get.mockResolvedValue({ templates: unchanged });
+    store.set.mockReturnValue(new Promise((resolve) => {
+      // Create a new async function that resolves the promise returned from
+      // calls to `store.set()` and returns a promise itself which we can wait
+      // on to ensure data saving has been handled.
+      saved = async function save() { resolve(); };
+    }));
+
+    const wrapper = render({ store });
+
+    change(wrapper, 'branch', 'branch++');
+    change(wrapper, 'commit', 'commit++');
+    change(wrapper, 'command', 'command++');
 
     const event = { preventDefault: jest.fn() };
     wrapper.simulate('submit', event);
 
     expect(event.preventDefault).toHaveBeenCalled();
 
-    const templates = { branch: 'branch++', commit: 'commit++', command: 'command++' };
-    expect(store.set).toHaveBeenCalledWith({ templates }, instance.handleSaved);
+    const changed = { branch: 'branch++', commit: 'commit++', command: 'command++' };
+    expect(store.set).toHaveBeenCalledWith({ templates: changed });
+
     expect(wrapper.find('button[type="submit"]').prop('disabled')).toBe(true);
     expect(inputs(wrapper).every({ disabled: true })).toBe(true);
-    expect(instance.handleSaved).toHaveBeenCalled();
-  });
 
-  it('re-enables the form elements once templates are stored', () => {
-    const { wrapper, instance } = render({});
-
-    wrapper.simulate('submit', { preventDefault: () => {} });
-    instance.handleSaved();
-    wrapper.update();
+    await saved();
 
     expect(wrapper.find('button[type="submit"]').prop('disabled')).toBe(false);
     expect(inputs(wrapper).every({ disabled: false })).toBe(true);
