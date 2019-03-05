cbfcdaf9145d33ad3db1d0b60200dcbda409b6b6
nickgnd <nicolo@bitcrowd.net>
2019-03-05 14:59:35 +0100

    Update Trello adapter to use REST API

--- src/common/adapters/trello.test.js
+++ src/common/adapters/trello.test.js
@@ -1,28 +1,53 @@
-import { JSDOM } from 'jsdom';
-
+import client from '../client';
 import scan from './trello';
 
-const CARD = `
-  <div class="card-detail-window">
-    <h2 class="card-detail-title-assist">A Trello Card</h2>
-  </div>
-`;
+import loc from './__helpers__/location';
+
+jest.mock('../client', () => jest.fn());
+
+const key = 'haKn65Sy';
+
+const idShort = '123';
+const name = 'A quick summary of the card';
+const slug = '123-a-quick-summary-of-the-card';
+
+const response = { idShort, name };
 
 describe('trello adapter', () => {
-  const loc = { host: 'trello.com', pathname: '/c/kkRPwRqw/89-a-trello-card' };
+  const api = { get: jest.fn() };
 
-  function doc(body = '') {
-    const { window } = new JSDOM(`<html><body>${body}</body></html>`);
-    return window.document;
-  }
+  beforeEach(() => {
+    api.get.mockReturnValue({ json: () => response });
+    client.mockReturnValue(api);
+  });
 
-  it('returns null if it is on a different page', async () => {
-    const result = await scan({ host: 'other.com' }, doc(CARD));
+  afterEach(() => {
+    api.get.mockReset();
+    client.mockReset();
+  });
+
+  it('returns null when on a different host', async () => {
+    const result = await scan(loc('another-domain.com'));
+    expect(api.get).not.toHaveBeenCalled();
     expect(result).toBe(null);
   });
 
-  it('extracts tickets from trello cards', async () => {
-    const result = await scan(loc, doc(CARD));
-    expect(result).toEqual([{ id: '89', title: 'A Trello Card', type: 'feature' }]);
+  it('returns null when on a different view', async () => {
+    const result = await scan(loc('trello.com', '/another/url'));
+    expect(api.get).not.toHaveBeenCalled();
+    expect(result).toBe(null);
+  });
+
+  it('uses the correct endpoint', async () => {
+    await scan(loc('trello.com', '/c/aghy1jdk/1-ticket-title'));
+    expect(client).toHaveBeenCalledWith('https://trello.com/1');
+    expect(api.get).toHaveBeenCalled();
+  });
+
+  it('extracts tickets from the current card', async () => {
+    const result = await scan(loc('trello.com', `/c/${key}/${slug}`));
+    expect(client).toHaveBeenCalledWith('https://trello.com/1');
+    expect(api.get).toHaveBeenCalledWith(`cards/${key}`);
+    expect(result).toEqual([{ id: idShort, title: name, type: 'feature' }]);
   });
 });
