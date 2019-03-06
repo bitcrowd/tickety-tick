c1829a6e241bee8cb405bc911334d05d56e01635
pmeinhardt <mnhrdt@gmail.com>
2019-03-06 09:09:39 +0100

    Small Jira adapter test refactoring

--- src/common/adapters/jira.test.js
+++ src/common/adapters/jira.test.js
@@ -3,19 +3,21 @@ import scan from './jira';
 
 jest.mock('../client', () => jest.fn());
 
-const selectedIssue = 'RC-654';
-const issueType = 'Story';
-const title = 'A quick summary of the ticket';
+const key = 'RC-654';
 
 const response = {
   id: '10959',
-  key: selectedIssue,
   fields: {
-    issuetype: {
-      name: issueType,
-    },
-    summary: title,
+    issuetype: { name: 'Story' },
+    summary: 'A quick summary of the ticket',
   },
+  key,
+};
+
+const ticket = {
+  id: response.key,
+  title: response.fields.summary,
+  type: response.fields.issuetype.name.toLowerCase(),
 };
 
 describe('jira adapter', () => {
@@ -50,42 +52,27 @@ describe('jira adapter', () => {
     expect(result).toBe(null);
   });
 
-  it('returns null when the selected issue is missing', async () => {
+  it('returns null when no issue is selected', async () => {
     const result = await scan(loc('my-subdomain.atlassian.com'));
     expect(api.get).not.toHaveBeenCalled();
     expect(result).toBe(null);
   });
 
   it('extracts tickets from the active sprints tab', async () => {
-    const result = await scan(loc('my-subdomain.atlassian.net', '', { selectedIssue }));
-
-    expect(api.get).toHaveBeenCalledWith(`issue/${selectedIssue}`);
-    expect(result).toEqual([{
-      id: selectedIssue,
-      title,
-      type: issueType.toLowerCase(),
-    }]);
+    const result = await scan(loc('my-subdomain.atlassian.net', '', { selectedIssue: key }));
+    expect(api.get).toHaveBeenCalledWith(`issue/${key}`);
+    expect(result).toEqual([ticket]);
   });
 
   it('extracts tickets from the issues tab', async () => {
-    const result = await scan(loc('my-subdomain.atlassian.net', `/projects/RC/issues/${selectedIssue}`, { filter: 'something' }));
-
-    expect(api.get).toHaveBeenCalledWith(`issue/${selectedIssue}`);
-    expect(result).toEqual([{
-      id: selectedIssue,
-      title,
-      type: issueType.toLowerCase(),
-    }]);
+    const result = await scan(loc('my-subdomain.atlassian.net', `/projects/RC/issues/${key}`, { filter: 'something' }));
+    expect(api.get).toHaveBeenCalledWith(`issue/${key}`);
+    expect(result).toEqual([ticket]);
   });
 
   it('extracts tickets when browsing an issue', async () => {
-    const result = await scan(loc('my-subdomain.atlassian.net', `/browse/${selectedIssue}`));
-
-    expect(api.get).toHaveBeenCalledWith(`issue/${selectedIssue}`);
-    expect(result).toEqual([{
-      id: selectedIssue,
-      title,
-      type: issueType.toLowerCase(),
-    }]);
+    const result = await scan(loc('my-subdomain.atlassian.net', `/browse/${key}`));
+    expect(api.get).toHaveBeenCalledWith(`issue/${key}`);
+    expect(result).toEqual([ticket]);
   });
 });
