7f52c036a65e51872f7cbdba7ec9d8f02b946da6
pmeinhardt <mnhrdt@gmail.com>
2019-03-16 12:41:22 +0100

    Use named exports for adapter helpers

--- src/common/adapters/helpers.js
+++ src/common/adapters/helpers.js
@@ -1,20 +1,20 @@
-function trim(string) {
+export function trim(string) {
   return string ? string.replace(/^\s+|\s+$/g, '') : null;
 }
 
 // Finders
 
-function $find(selector, context) {
+export function $find(selector, context) {
   return context.querySelector(selector);
 }
 
-function $all(selector, context) {
+export function $all(selector, context) {
   return Array.from(context.querySelectorAll(selector));
 }
 
 // Traversal
 
-function $closest(selector, context) {
+export function $closest(selector, context) {
   if (typeof context.closest !== 'function') {
     // Shim Element.closest for browsers that do not support it (and jsdom):
     // https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
@@ -33,42 +33,29 @@ function $closest(selector, context) {
 
 // Properties
 
-function $attr(selector, context, key) {
+export function $attr(selector, context, key) {
   const node = $find(selector, context);
   return node ? node.getAttribute(key) : null;
 }
 
-function $classed(node, cls) {
+export function $classed(node, cls) {
   return node.classList.contains(cls);
 }
 
-function $data(node, key) {
+export function $data(node, key) {
   return node.dataset[key];
 }
 
-function $has(selector, context) {
+export function $has(selector, context) {
   return $find(selector, context) !== null;
 }
 
-function $text(selector, context) {
+export function $text(selector, context) {
   const node = $find(selector, context);
   return node ? trim(node.textContent) : null;
 }
 
-function $value(selector, context) {
+export function $value(selector, context) {
   const node = $find(selector, context);
   return node ? trim(node.value || node.getAttribute('value')) : null;
 }
-
-export {
-  $all,
-  $attr,
-  $classed,
-  $closest,
-  $data,
-  $find,
-  $has,
-  $text,
-  $value,
-  trim,
-};
