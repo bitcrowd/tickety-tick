66632ee76a7a44b922e0bd7612dce5f4aac35e7f
pmeinhardt <mnhrdt@gmail.com>
2019-03-17 16:58:55 +0100

    Render error report if a problem was encountered

--- src/common/popup/components/no-tickets.jsx
+++ src/common/popup/components/no-tickets.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import ExternalLink from './external-link';
 
-const href = 'https://github.com/bitcrowd/tickety-tick/issues';
+import ErrorShape from '../utils/error-shape';
 
-function NoTickets() {
+function IssueLink() {
   return (
-    <div className="text-center">
+    <ExternalLink href="https://github.com/bitcrowd/tickety-tick/issues">
+      Report an issue
+    </ExternalLink>
+  );
+}
+
+function Hint() {
+  return (
+    <>
       <h5 className="mt-3 mb-3">No tickets found on this page.</h5>
       <h6>
         Did you select or open any tickets?
@@ -20,12 +29,49 @@ function NoTickets() {
         Missing anything or found a bug?
       </h6>
       <p className="pb-1">
-        <ExternalLink href={href}>Report an issue</ExternalLink>
+        <IssueLink />
       </p>
+    </>
+  );
+}
+
+function Report({ errors }) {
+  return (
+    <>
+      <h5 className="mt-3 mb-3">Houston, we have a problem.</h5>
+      <p>
+        Tickety-Tick encountered
+        {' '}
+        <strong>
+          {errors.length}
+          {' '}
+          {errors.length === 1 ? 'error' : 'errors'}
+        </strong>
+        <br />
+        while scanning for tickets.
+      </p>
+      <p className="pb-1">
+        <IssueLink />
+      </p>
+    </>
+  );
+}
+
+Report.propTypes = {
+  errors: PropTypes.arrayOf(ErrorShape).isRequired,
+};
+
+function NoTickets({ errors }) {
+  return (
+    <div className="text-center">
+      {errors.length > 0 ? <Report errors={errors} /> : <Hint />}
     </div>
   );
 }
 
-NoTickets.propTypes = {};
+NoTickets.propTypes = {
+  errors: PropTypes.arrayOf(ErrorShape).isRequired,
+};
 
+export { Hint, Report };
 export default NoTickets;
