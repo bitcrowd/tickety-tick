d35f7b59a3138c6504103dc36fa4362d3af8795b
Bodo Tasche <bodo@wannawork.de>
2019-05-30 16:16:56 +0200

    [#154] Github tickety-tick issues seem to not work anymore

--- src/common/adapters/github.js
+++ src/common/adapters/github.js
@@ -5,14 +5,33 @@ import {
   $value,
 } from './helpers';
 
-async function scan(loc, doc) {
-  if ($has('.issues-listing .js-issue-row.selected', doc)) {
-    const issues = $all('.issues-listing .js-issue-row.selected', doc);
+// Element classes on GitHub have slightly changed over time.
+// To maintain support for some older GitHub Enterprise instances,
+// the adapter will attempt different versions of selectors.
+export const selectors = {
+  default: {
+    issuesPage: '.js-check-all-container .js-issue-row.selected',
+    issuesPageLabel: '.labels .IssueLabel',
+    issuePage: '.js-issues-results .gh-header-number',
+    issuePageLabel: '.js-issue-labels .sidebar-labels-style[title="bug"]',
+  },
+  legacy: {
+    issuesPage: '.issues-listing .js-issue-row.selected',
+    issuesPageLabel: '.labels .label',
+    issuePage: '.issues-listing .gh-header-number',
+    issuePageLabel: '.sidebar-labels .label[title="bug"]',
+  },
+};
+
+async function attempt(doc, selector) {
+  // issue list page
+  if ($has(selector.issuesPage, doc)) {
+    const issues = $all(selector.issuesPage, doc);
 
     const tickets = issues.map((issue) => {
       const id = $value('input.js-issues-list-check', issue);
       const title = $text('a.js-navigation-open', issue);
-      const labels = $all('.labels .label', issue);
+      const labels = $all(selector.issuesPageLabel, issue);
       const type = labels.some(l => /bug/i.test(l.textContent)) ? 'bug' : 'feature';
 
       return { id, title, type };
@@ -21,15 +40,16 @@ async function scan(loc, doc) {
     return tickets;
   }
 
-  if ($has('.issues-listing .gh-header-number', doc)) {
+  // single issue page
+  if ($has(selector.issuePage, doc)) {
     const id = $text('.gh-header-number', doc).replace(/^#/, '');
     const title = $text('.js-issue-title', doc);
-    const type = $has('.sidebar-labels .label[title="bug"]', doc) ? 'bug' : 'feature';
+    const type = $has(selector.issuePageLabel, doc) ? 'bug' : 'feature';
     const tickets = [{ id, title, type }];
     return tickets;
   }
 
-  // github project
+  // project page
   if ($has('.project-columns .project-card', doc)) {
     const openProjectCardSelector = '.project-columns .project-card[data-card-state=\'["open"]\']';
     const projectCards = $all(openProjectCardSelector, doc);
@@ -37,7 +57,9 @@ async function scan(loc, doc) {
     const tickets = projectCards.map((card) => {
       const id = JSON.parse(card.dataset.cardTitle).slice(-2, -1)[0];
       const title = $text('a.h5', card);
-      const type = JSON.parse(card.dataset.cardLabel).includes('bug') ? 'bug' : 'feature';
+      const type = JSON.parse(card.dataset.cardLabel).includes('bug')
+        ? 'bug'
+        : 'feature';
 
       return { id, title, type };
     });
@@ -46,6 +68,12 @@ async function scan(loc, doc) {
   }
 
   return [];
+}
+
+async function scan(loc, doc) {
+  const tickets = await attempt(doc, selectors.default);
+  if (tickets.length > 0) return tickets;
+  return attempt(doc, selectors.legacy);
 }
 
 export default scan;
--- src/common/adapters/github.test.js
+++ src/common/adapters/github.test.js
@@ -1,105 +1,175 @@
 import { JSDOM } from 'jsdom';
+import scan, { selectors } from './github';
 
-import scan from './github';
+const pages = {
+  default: {
+    issuepage: `
+      <div class="js-issues-results">
+        <h1 class="gh-header-title">
+          <span class="js-issue-title">A Random GitHub Issue</span>
+          <span class="gh-header-number">#12</span>
+        </h1>
+      </div>`,
+    bugpage: `
+      <div class="js-issues-results">
+        <h1 class="gh-header-title">
+          <span class="js-issue-title">A Random GitHub Issue</span>
+          <span class="gh-header-number">#12</span>
+        </h1>
+        <div class="js-issue-labels">
+          <a class="sidebar-labels-style" title="bug">bug</a>
+        </div>
+      </div>`,
+    indexpage: `
+        <div class="js-check-all-container">
+          <ul>
+            <li id="issue_12" class="js-issue-row selected">
+              <input type="checkbox" class="js-issues-list-check" name="issues[]" value="12">
+              <a href="/bitcrowd/tickety-tick/issues/12" class="h4 js-navigation-open">
+                A Selected GitHub Issue
+              </a>
+              <span class="labels">
+                <a href="#" class="IssueLabel">bug</a>
+              </span>
+            </li>
+            <li id="issue_11" class="js-issue-row">
+              <input type="checkbox" class="js-issues-list-check" name="issues[]" value="11">
+              <a href="/bitcrowd/tickety-tick/issues/11" class="h4 js-navigation-open">
+                A GitHub Issue
+              </a>
+              <span class="labels"></span>
+            </li>
+          </ul>
+        </div>`,
+    projectpage: `
+        <div class="project-columns">
+          <div class="project-card"
+              data-card-state='["open"]'
+              data-card-label='["enhancement"]'
+              data-card-title='["an","example","feature","ticket","42","#42"]'>
+            <a class="h5">An Example Feature Ticket</a>
+          </div>
+          <div class="project-card"
+              data-card-state='["open"]'
+              data-card-label='["bug"]'
+              data-card-title='["an","example","bug","ticket","43","#43"]'>
+            <a class="h5">An Example Bug Ticket</a>
+          </div>
+          <div class="project-card"
+              data-card-state='["closed"]'
+              data-card-label='["bug"]'
+              data-card-title='["an","example","bug","ticket","which","was","closed","and","should","not","be","found","44","#44"]'>
+            <a class="h5">An Example Bug Ticket which was closed and should not be found</a>
+          </div>
+        </div>`,
+  },
+  legacy: {
+    issuepage: `
+      <div class="issues-listing">
+        <h1 class="gh-header-title">
+          <span class="js-issue-title">A Random GitHub Issue</span>
+          <span class="gh-header-number">#12</span>
+        </h1>
+      </div>`,
 
-const ISSUEPAGE = `
-  <div class="issues-listing">
-    <h1 class="gh-header-title">
-      <span class="js-issue-title">A Random GitHub Issue</span>
-      <span class="gh-header-number">#12</span>
-    </h1>
-  </div>
-`;
+    bugpage: `
+      <div class="issues-listing">
+        <h1 class="gh-header-title">
+          <span class="js-issue-title">A Random GitHub Issue</span>
+          <span class="gh-header-number">#12</span>
+        </h1>
+        <div class="sidebar-labels">
+          <a class="label" title="bug">bug</a>
+        </div>
+      </div>`,
 
-const BUGPAGE = `
-  <div class="issues-listing">
-    <h1 class="gh-header-title">
-      <span class="js-issue-title">A Random GitHub Issue</span>
-      <span class="gh-header-number">#12</span>
-    </h1>
-    <div class="sidebar-labels">
-      <a class="label" title="bug">bug</a>
-    </div>
-  </div>
-`;
+    indexpage: `
+      <div class="issues-listing">
+        <ul>
+          <li id="issue_12" class="js-issue-row selected">
+            <input type="checkbox" class="js-issues-list-check" name="issues[]" value="12">
+            <a href="/bitcrowd/tickety-tick/issues/12" class="h4 js-navigation-open">
+              A Selected GitHub Issue
+            </a>
+            <span class="labels">
+              <a href="#" class="label">bug</a>
+            </span>
+          </li>
+          <li id="issue_11" class="js-issue-row">
+            <input type="checkbox" class="js-issues-list-check" name="issues[]" value="11">
+            <a href="/bitcrowd/tickety-tick/issues/11" class="h4 js-navigation-open">
+              A GitHub Issue
+            </a>
+            <span class="labels"></span>
+          </li>
+        </ul>
+      </div>`,
 
-const INDEXPAGE = `
-  <div class="issues-listing">
-    <ul>
-      <li id="issue_12" class="js-issue-row selected">
-        <input type="checkbox" class="js-issues-list-check" name="issues[]" value="12">
-        <a href="/bitcrowd/tickety-tick/issues/12" class="h4 js-navigation-open">
-          A Selected GitHub Issue
-        </a>
-        <span class="labels">
-          <a href="#" class="label">bug</a>
-        </span>
-      </li>
-      <li id="issue_11" class="js-issue-row">
-        <input type="checkbox" class="js-issues-list-check" name="issues[]" value="11">
-        <a href="/bitcrowd/tickety-tick/issues/11" class="h4 js-navigation-open">
-          A GitHub Issue
-        </a>
-        <span class="labels"></span>
-      </li>
-    </ul>
-  </div>
-`;
+    projectpage: `
+      <div class="project-columns">
+        <div class="project-card"
+            data-card-state='["open"]'
+            data-card-label='["enhancement"]'
+            data-card-title='["an","example","feature","ticket","42","#42"]'>
+          <a class="h5">An Example Feature Ticket</a>
+        </div>
+        <div class="project-card"
+            data-card-state='["open"]'
+            data-card-label='["bug"]'
+            data-card-title='["an","example","bug","ticket","43","#43"]'>
+          <a class="h5">An Example Bug Ticket</a>
+        </div>
+        <div class="project-card"
+            data-card-state='["closed"]'
+            data-card-label='["bug"]'
+            data-card-title='["an","example","bug","ticket","which","was","closed","and","should","not","be","found","44","#44"]'>
+          <a class="h5">An Example Bug Ticket which was closed and should not be found</a>
+        </div>
+      </div>`,
+  },
+};
 
-const PROJECTPAGE = `
-  <div class="project-columns">
-    <div class="project-card"
-         data-card-state='["open"]'
-         data-card-label='["enhancement"]'
-         data-card-title='["an","example","feature","ticket","42","#42"]'>
-      <a class="h5">An Example Feature Ticket</a>
-    </div>
-    <div class="project-card"
-         data-card-state='["open"]'
-         data-card-label='["bug"]'
-         data-card-title='["an","example","bug","ticket","43","#43"]'>
-      <a class="h5">An Example Bug Ticket</a>
-    </div>
-    <div class="project-card"
-         data-card-state='["closed"]'
-         data-card-label='["bug"]'
-         data-card-title='["an","example","bug","ticket","which","was","closed","and","should","not","be","found","44","#44"]'>
-      <a class="h5">An Example Bug Ticket which was closed and should not be found</a>
-    </div>
-  </div>
-`;
+Object.keys(selectors).forEach((variant) => {
+  const html = pages[variant];
 
-describe('github adapter', () => {
-  function dom(body = '') {
-    const { window } = new JSDOM(`<html><body>${body}</body></html>`);
-    return window.document;
-  }
+  describe(`github adapter (${variant})`, () => {
+    function dom(body = '') {
+      const { window } = new JSDOM(`<html><body>${body}</body></html>`);
+      return window.document;
+    }
 
-  it('returns an empty array if it is on a different page', async () => {
-    const result = await scan(null, dom());
-    expect(result).toEqual([]);
-  });
+    it('returns an empty array if it is on a different page', async () => {
+      const result = await scan(null, dom());
+      expect(result).toEqual([]);
+    });
 
-  it('extracts tickets from issue pages', async () => {
-    const result = await scan(null, dom(ISSUEPAGE));
-    expect(result).toEqual([{ id: '12', title: 'A Random GitHub Issue', type: 'feature' }]);
-  });
+    it('extracts tickets from issue pages', async () => {
+      const result = await scan(null, dom(html.issuepage));
+      expect(result).toEqual([
+        { id: '12', title: 'A Random GitHub Issue', type: 'feature' },
+      ]);
+    });
 
-  it('recognizes issues labelled as bugs', async () => {
-    const result = await scan(null, dom(BUGPAGE));
-    expect(result).toEqual([{ id: '12', title: 'A Random GitHub Issue', type: 'bug' }]);
-  });
+    it('recognizes issues labelled as bugs', async () => {
+      const result = await scan(null, dom(html.bugpage));
+      expect(result).toEqual([
+        { id: '12', title: 'A Random GitHub Issue', type: 'bug' },
+      ]);
+    });
 
-  it('extracts tickets from issues index pages', async () => {
-    const result = await scan(null, dom(INDEXPAGE));
-    expect(result).toEqual([{ id: '12', title: 'A Selected GitHub Issue', type: 'bug' }]);
-  });
+    it('extracts tickets from issues index pages', async () => {
+      const result = await scan(null, dom(html.indexpage));
+      expect(result).toEqual([
+        { id: '12', title: 'A Selected GitHub Issue', type: 'bug' },
+      ]);
+    });
 
-  it('extracts tickets from project pages', async () => {
-    const result = await scan(null, dom(PROJECTPAGE));
-    expect(result).toEqual([
-      { id: '42', title: 'An Example Feature Ticket', type: 'feature' },
-      { id: '43', title: 'An Example Bug Ticket', type: 'bug' },
-    ]);
+    it('extracts tickets from project pages', async () => {
+      const result = await scan(null, dom(html.projectpage));
+      expect(result).toEqual([
+        { id: '42', title: 'An Example Feature Ticket', type: 'feature' },
+        { id: '43', title: 'An Example Bug Ticket', type: 'bug' },
+      ]);
+    });
   });
 });
