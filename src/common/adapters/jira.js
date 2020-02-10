b3827b6e1f9cdd41b3c08b110779eddabf37ca57
qclin <qiaoclin@gmail.com>
2020-02-10 14:12:03 +0100

    rever autoformating of single quotes

--- src/common/adapters/jira.js
+++ src/common/adapters/jira.js
@@ -9,29 +9,29 @@
 // * Issues and filters: https://<YOUR-SUBDOMAIN>.atlassian.net/projects/<PROJECT-KEY>/issues/<ISSUE-KEY>
 // * Issue view: https://<YOUR-SUBDOMAIN>.atlassian.net/browse/<ISSUE-KEY>
 
-import { match } from "micro-match";
+import { match } from 'micro-match';
 
-import client from "../client";
+import client from '../client';
 
 function isJiraPage(loc, doc) {
-  if (loc.host.endsWith(".atlassian.net")) return true;
-  if (doc.body.id === "jira") return true; // self-managed instance on different domain
+  if (loc.host.endsWith('.atlassian.net')) return true;
+  if (doc.body.id === 'jira') return true; // self-managed instance on different domain
   return false;
 }
 
-const pathSuffixes = new RegExp("/(browse/[^/]+|projects/[^/]+/issues/[^/]+|secure/RapidBoard.jspa|jira/software/projects/[^/]+/boards/[^/]+/)$", "g");
+const pathSuffixes = new RegExp('/(browse/[^/]+|projects/[^/]+/issues/[^/]+|secure/RapidBoard.jspa|jira/software/projects/[^/]+/boards/[^/]+/)$', 'g');
 function getPathPrefix(loc) {
-  return loc.pathname.replace(pathSuffixes, "");
+  return loc.pathname.replace(pathSuffixes, '');
 }
 
-function getSelectedIssueId(loc, prefix = "") {
+function getSelectedIssueId(loc, prefix = '') {
   const { searchParams: params } = new URL(loc.href);
 
-  if (params.has("selectedIssue")) return params.get("selectedIssue");
+  if (params.has('selectedIssue')) return params.get('selectedIssue');
 
   const path = loc.pathname.substr(prefix.length); // strip path prefix
 
-  return (["/projects/:project/issues/:id", "/browse/:id"]
+  return (['/projects/:project/issues/:id', '/browse/:id']
     .map((pattern) => match(pattern, path).id)
     .find(Boolean));
 }
