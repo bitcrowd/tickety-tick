89f4afe7fa300dffde86036439d4bba7e9b162cc
pmeinhardt <mnhrdt@gmail.com>
2020-04-16 12:03:45 +0200

    Clean up tests

--- src/common/popup/components/ticket-controls.test.jsx
+++ src/common/popup/components/ticket-controls.test.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 import { ticket as make } from '../../../../test/factories';
 import TicketControls from './ticket-controls';
 
-describe('<TicketControls />', () => {
-  function subject(overrides) {
+describe('ticket-controls', () => {
+  function render(overrides) {
     const defaults = {
       tickets: ['raz', 'dva', 'tri'].map((title, i) =>
         make({ id: `${i + 1}`, title })
@@ -16,6 +16,6 @@ describe('<TicketControls />', () => {
   }
 
   it('renders', () => {
-    expect(() => subject({})).not.toThrow();
+    expect(() => render({})).not.toThrow();
   });
 });
