ad290f91086fa4d98eb37a3495246939a80c5167
Max Mulatz <max@bitcrowd.net>
2020-04-28 18:22:07 +0200

    Update tests for CopyButton component
    Add tests to ensure the `close` function runs after the value is copied.

--- src/common/popup/components/copy-button.test.jsx
+++ src/common/popup/components/copy-button.test.jsx
@@ -8,6 +8,7 @@ jest.mock('react', () => ({
   ...jest.requireActual('react'),
   useContext: jest.fn(),
 }));
+jest.useFakeTimers();
 
 describe('copy-button', () => {
   function render(overrides, env) {
@@ -18,10 +19,16 @@ describe('copy-button', () => {
   }
 
   let pbcopy;
+  let close;
 
   beforeEach(() => {
     useContext.mockReset();
     pbcopy = jest.fn();
+    close = jest.fn();
+  });
+
+  afterEach(() => {
+    jest.clearAllTimers();
   });
 
   it('uses the env context', () => {
@@ -29,11 +36,38 @@ describe('copy-button', () => {
     expect(useContext).toHaveBeenCalledWith(EnvContext);
   });
 
+  it('renders its children', () => {
+    const children = 'button content';
+    const wrapper = render({ children }, { pbcopy });
+    expect(wrapper.contains(children)).toBe(true);
+  });
+
+  it('passes the "copied" state to its children if they are a function', () => {
+    const children = jest.fn();
+    const wrapper = render({ children }, { pbcopy });
+    expect(children).toHaveBeenNthCalledWith(1, false);
+
+    wrapper.simulate('click');
+
+    expect(children).toHaveBeenNthCalledWith(2, true);
+  });
+
   it('calls the context pbcopy function with the provided value on click', () => {
     const value = 'a lot of value for such a small button';
     const wrapper = render({ value }, { pbcopy });
     wrapper.simulate('click');
     expect(pbcopy).toHaveBeenCalledWith(value);
+  });
+
+  it('calls the context close function delayed after the value was copied', () => {
+    const value = 'a lot of value for such a small button';
+    const wrapper = render({ value }, { pbcopy, close });
+    wrapper.simulate('click');
+    expect(pbcopy).toHaveBeenCalledWith(value);
+    expect(close).not.toHaveBeenCalled();
+
+    jest.advanceTimersByTime(700);
+    expect(close).toHaveBeenCalled();
   });
 
   it('passes on any other properties to the rendered button', () => {
