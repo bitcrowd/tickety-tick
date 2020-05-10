2dcbce64768ab3af8a3ba8a548ddbb69249d7642
pmeinhardt <mnhrdt@gmail.com>
2020-05-10 23:11:13 +0200

    Remove unnecessary JSDOM from test setup code

--- test/setup.js
+++ test/setup.js
@@ -1,9 +1,4 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { JSDOM } from 'jsdom';
 
 Enzyme.configure({ adapter: new Adapter() });
-
-global.window = new JSDOM('<html><body></body></html>').window;
-global.document = global.window.document;
-global.navigator = global.window.navigator;
