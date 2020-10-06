3f1bcde65cc6b3bbd99eef146dcd7b54cd0cf1f2
Niklas Klein <mail@taig.io>
2020-10-06 16:21:56 +0200

    [#270] Fix GitLab integration (#271)
    Update GitLab adapter to support the new page source.
    
    closes #270

--- src/core/adapters/gitlab.js
+++ src/core/adapters/gitlab.js
@@ -6,14 +6,26 @@
 
 import { $has, $text } from './helpers';
 
+function findTicketId(doc) {
+  const ticketId = doc.body.dataset.pageTypeId;
+
+  if (ticketId) {
+    return ticketId;
+  }
+
+  // Legacy approach of extracting the ticket id, left in place to support
+  // older self-hosted GitLab installations
+  const initialDataEl = doc.getElementById('js-issuable-app-initial-data');
+  const initialData = JSON.parse(
+    initialDataEl.innerHTML.replace(/&quot;/g, '"')
+  );
+
+  return initialData.issuableRef.match(/#(\d+)/)[1];
+}
+
 async function scan(loc, doc) {
   if (doc.body.dataset.page === 'projects:issues:show') {
-    const initialDataEl = doc.getElementById('js-issuable-app-initial-data');
-    const initialData = JSON.parse(
-      initialDataEl.innerHTML.replace(/&quot;/g, '"')
-    );
-
-    const id = initialData.issuableRef.match(/#(\d+)/)[1];
+    const id = findTicketId(doc);
     const title = $text('.issue-details .title', doc);
     const type = $has('.labels [data-original-title="bug"]', doc)
       ? 'bug'
