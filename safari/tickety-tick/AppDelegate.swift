3560797329beb98e46ddf81d5fa04c54222e32d2
Paul Meinhardt <paul@bitcrowd.net>
2021-08-12 07:30:49 +0000

    Clean up Safari extension code (#312)
    * Simplify Swift file header, configure template
    * Format Swift code using SwiftFormat
    * Configure CI check for Swift file formatting

--- safari/tickety-tick/AppDelegate.swift
+++ safari/tickety-tick/AppDelegate.swift
@@ -1,25 +1,20 @@
 //
-//  AppDelegate.swift
-//  tickety-tick
-//
-//  Created by Paul Meinhardt on 25/11/20.
+// AppDelegate.swift
 //
 
 import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
+  func applicationDidFinishLaunching(_: Notification) {
+    // Insert code here to initialize your application
+  }
 
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        // Insert code here to initialize your application
-    }
+  func applicationWillTerminate(_: Notification) {
+    // Insert code here to tear down your application
+  }
 
-    func applicationWillTerminate(_ notification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-        return true
-    }
-
+  func applicationShouldTerminateAfterLastWindowClosed(_: NSApplication) -> Bool {
+    true
+  }
 }
--- safari/tickety-tick/ViewController.swift
+++ safari/tickety-tick/ViewController.swift
@@ -1,8 +1,5 @@
 //
-//  ViewController.swift
-//  tickety-tick
-//
-//  Created by Paul Meinhardt on 25/11/20.
+// ViewController.swift
 //
 
 import Cocoa
@@ -13,39 +10,41 @@ let appName = "tickety-tick"
 let extensionBundleIdentifier = "net.bitcrowd.tickety-tick.Extension"
 
 class ViewController: NSViewController {
+  @IBOutlet var appNameLabel: NSTextField!
 
-    @IBOutlet var appNameLabel: NSTextField!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.appNameLabel.stringValue = appName
-        SFSafariExtensionManager.getStateOfSafariExtension(withIdentifier: extensionBundleIdentifier) { (state, error) in
-            guard let state = state, error == nil else {
-                // Insert code to inform the user that something went wrong.
-                return
-            }
-
-            DispatchQueue.main.async {
-                if (state.isEnabled) {
-                    self.appNameLabel.stringValue = "\(appName)'s extension is currently on."
-                } else {
-                    self.appNameLabel.stringValue = "\(appName)'s extension is currently off. You can turn it on in Safari Extensions preferences."
-                }
-            }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    appNameLabel.stringValue = appName
+    SFSafariExtensionManager
+      .getStateOfSafariExtension(withIdentifier: extensionBundleIdentifier) { state, error in
+        guard let state = state, error == nil else {
+          // Insert code to inform the user that something went wrong.
+          return
         }
-    }
-    
-    @IBAction func openSafariExtensionPreferences(_ sender: AnyObject?) {
-        SFSafariApplication.showPreferencesForExtension(withIdentifier: extensionBundleIdentifier) { error in
-            guard error == nil else {
-                // Insert code to inform the user that something went wrong.
-                return
-            }
 
-            DispatchQueue.main.async {
-                NSApplication.shared.terminate(nil)
-            }
+        DispatchQueue.main.async {
+          if state.isEnabled {
+            self.appNameLabel.stringValue = "\(appName)'s extension is currently on."
+          } else {
+            self.appNameLabel
+              .stringValue =
+              "\(appName)'s extension is currently off. You can turn it on in Safari Extensions preferences."
+          }
         }
-    }
+      }
+  }
 
+  @IBAction func openSafariExtensionPreferences(_: AnyObject?) {
+    SFSafariApplication
+      .showPreferencesForExtension(withIdentifier: extensionBundleIdentifier) { error in
+        guard error == nil else {
+          // Insert code to inform the user that something went wrong.
+          return
+        }
+
+        DispatchQueue.main.async {
+          NSApplication.shared.terminate(nil)
+        }
+      }
+  }
 }
