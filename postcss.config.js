c4033390ab856b9a0eb7cbd3dedbe54e1899b473
Paul Meinhardt <paul@bitcrowd.net>
2021-09-30 07:40:27 +0200

    Set purged files based on source path

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,19 @@
 module.exports = {
   plugins: [
-    ["@fullhuman/postcss-purgecss", { content: ["src/**/*.{html,ts,tsx}"] }],
+    [
+      "@fullhuman/postcss-purgecss",
+      {
+        contentFunction: (src) => {
+          if (src.endsWith("src/popup/index.scss")) {
+            return ["src/popup/**/*.{html,ts,tsx}"];
+          }
+          if (src.endsWith("src/options/index.scss")) {
+            return ["src/options/**/*.{html,ts,tsx}"];
+          }
+          return ["src/**/*.{html,ts,tsx}"];
+        },
+      },
+    ],
     ["postcss-preset-env", {}],
     ["cssnano", { preset: "default" }],
   ],
