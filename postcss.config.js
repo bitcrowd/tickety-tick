da5a368986fb4a494535a7c6905f47c2e6055345
Paul Meinhardt <paul@bitcrowd.net>
2021-09-30 07:35:35 +0200

    Add postcss-purgecss plugin, trim CSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   plugins: [
+    ["@fullhuman/postcss-purgecss", { content: ["src/**/*.{html,ts,tsx}"] }],
     ["postcss-preset-env", {}],
     ["cssnano", { preset: "default" }],
   ],
