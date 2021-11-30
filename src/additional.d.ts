f728f99d2931cc7129ba0b5f2d37f4a6b5d14809
Paul Meinhardt <paul@bitcrowd.net>
2021-11-30 15:01:27 +0100

    Update type definitions
    - Use ".ts" rather than ".d.ts" extension
    - Update type documentation comments

--- src/additional.d.ts
+++ src/additional.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="react" />
 
+// Define type for ".svg" imports handled by "@svgr/webpack"
 declare module "*.svg" {
   export const ReactComponent: React.FunctionComponent<
     React.SVGProps<SVGSVGElement> & { title?: string }
@@ -12,5 +13,5 @@ declare module "*.svg" {
 // Unfortunately there are no type definitions for "micro-match" at the moment
 declare module "micro-match";
 
-// This global const is defined by Webpack DefinePlugin
+// This global const is defined by the Webpack DefinePlugin
 declare const COMMITHASH: string;
