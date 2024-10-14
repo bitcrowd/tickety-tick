a2baae04bb1209d7631d56e7572c85a757169390
Max Mulatz <max@bitcrowd.net>
2024-10-14 11:06:11 +0200

    Update release packages (#423)
    * Upgrade web-ext dependency
    
    * Update release script
    
    Use the new possibility to upload the human readable source code with
    the submission: https://extensionworkshop.com/documentation/develop/web-ext-command-reference/#whats-new
    
    For this to work, we need to download the Zip archive from GitHub first.
    
    * Add lint commands for the extension
    
    This works as a sanity check for whether the manifest file we generate
    is correct. There's unfortunately currently a bug in Chrome which
    reports a false error….
    
    * Update web extension polyfill
    
    * Update chrome-launcher package
    
    This required to change the open-in-chrome script to an ES module.
    
    * Update CLI for Chrome webstore uploads
    
    * Update types package
    
    * Update types

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,10 +1,11 @@
 import browser from "webextension-polyfill";
 
 import stdsearch from "../core/search";
+import type { ContentMessage } from "../types";
 
 if (window === window.top) {
-  browser.runtime.onMessage.addListener((msg) => {
-    if (msg.tickets) {
+  browser.runtime.onMessage.addListener((message: unknown) => {
+    if ((message as ContentMessage).tickets) {
       const url = new URL(window.location.toString());
       return stdsearch(url, document);
     }
