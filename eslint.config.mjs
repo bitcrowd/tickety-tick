bc00c5fb7fd23c3c415c2a9c776756b134c05454
Max Mulatz <max@bitcrowd.net>
2024-11-01 13:56:20 +0100

    Remove --ext CLI flag
    The extra file-extensions are now part of the config: https://eslint.org/docs/latest/use/configure/migration-guide#--ext

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
-import { fixupConfigRules } from "@eslint/compat";
+import { fixupConfigRules, includeIgnoreFile } from "@eslint/compat";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 import globals from "globals";
 import path from "node:path";
@@ -9,82 +9,111 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
+const gitignorePath = path.resolve(__dirname, ".gitignore");
 
-export default [...fixupConfigRules(compat.extends(
-    "airbnb",
-    "airbnb/hooks",
-    "plugin:import/typescript",
-    "plugin:jest/recommended",
-    "plugin:prettier/recommended",
-    "plugin:@typescript-eslint/recommended",
-)), {
+export default [
+  ...fixupConfigRules(
+    compat.extends(
+      "airbnb",
+      "airbnb/hooks",
+      "plugin:import/typescript",
+      "plugin:jest/recommended",
+      "plugin:prettier/recommended",
+      "plugin:@typescript-eslint/recommended",
+    ),
+  ),
+  includeIgnoreFile(gitignorePath),
+  {
+    files: ["**/*.ts", "**/*.tsx"],
     plugins: {
-        "simple-import-sort": simpleImportSort,
+      "simple-import-sort": simpleImportSort,
     },
 
     languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.jest,
-            COMMITHASH: "readonly",
-        },
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
+        COMMITHASH: "readonly",
+      },
     },
 
     rules: {
-        "@typescript-eslint/consistent-type-imports": ["error", {
-            prefer: "type-imports",
-        }],
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        {
+          prefer: "type-imports",
+        },
+      ],
 
-        "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
 
-        "@typescript-eslint/no-unused-vars": ["error", {
-            argsIgnorePattern: "^_",
-        }],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
 
-        "@typescript-eslint/no-use-before-define": "error",
+      "@typescript-eslint/no-use-before-define": "error",
 
-        "import/extensions": ["error", {
-            js: "never",
-            json: "always",
-            ts: "never",
-        }],
+      "import/extensions": [
+        "error",
+        {
+          js: "never",
+          json: "always",
+          ts: "never",
+        },
+      ],
 
-        "import/no-extraneous-dependencies": ["error", {
-            devDependencies: [
-                "webpack.config.ts",
-                "src/**/*.test.jsx",
-                "src/**/*.test.tsx",
-                "src/**/*.test.js",
-                "src/**/*.test.ts",
-                "test/**/*.js",
-                "test/**/*.ts",
-                "script/*",
-            ],
-        }],
+      "import/no-extraneous-dependencies": [
+        "error",
+        {
+          devDependencies: [
+            "webpack.config.ts",
+            "src/**/*.test.jsx",
+            "src/**/*.test.tsx",
+            "src/**/*.test.js",
+            "src/**/*.test.ts",
+            "test/**/*.js",
+            "test/**/*.ts",
+            "script/*",
+          ],
+        },
+      ],
 
-        "jsx-a11y/anchor-is-valid": ["error", {
-            specialLink: ["to"],
-        }],
+      "jsx-a11y/anchor-is-valid": [
+        "error",
+        {
+          specialLink: ["to"],
+        },
+      ],
 
-        "no-unused-vars": ["error", {
-            argsIgnorePattern: "^_",
-        }],
+      "no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
 
-        "no-use-before-define": "off",
-        "prettier/prettier": "error",
+      "no-use-before-define": "off",
+      "prettier/prettier": "error",
 
-        "react/jsx-filename-extension": ["error", {
-            extensions: [".js", ".jsx", ".ts", ".tsx"],
-        }],
+      "react/jsx-filename-extension": [
+        "error",
+        {
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+        },
+      ],
 
-        "react/jsx-props-no-spreading": "off",
-        "react/require-default-props": "off",
-        "react/prop-types": "off",
-        "simple-import-sort/exports": "error",
-        "simple-import-sort/imports": "error",
+      "react/jsx-props-no-spreading": "off",
+      "react/require-default-props": "off",
+      "react/prop-types": "off",
+      "simple-import-sort/exports": "error",
+      "simple-import-sort/imports": "error",
     },
-}];
+  },
+];
