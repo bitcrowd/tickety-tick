4514e822d2339547adb86e9f54e7171020421c9a
Max Mulatz <max@bitcrowd.net>
2025-10-16 21:52:47 +0200

    Update React

--- src/popup/components/about.test.tsx
+++ src/popup/components/about.test.tsx
@@ -5,6 +5,14 @@ import * as router from "../../../test/router";
 import type { Props } from "./about";
 import About from "./about";
 
+// NOTE: This is a temporary workaround for the tests not being
+// fully compatible with svgr while updating to React 19.
+jest.mock("../../icons/icon.svg", () => ({
+  ReactComponent: (props: React.SVGProps<SVGSVGElement>) => (
+    <svg {...props}>icon.svg</svg>
+  ),
+}));
+
 describe("about", () => {
   function subject(props: Props) {
     return render(<About {...props} />, { wrapper: router.wrapper });
