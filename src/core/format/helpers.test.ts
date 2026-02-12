87b5d08780d35de6b001a9a8d404a4588e1e5616
Ilya Bayaliyev <ilyabayaliyev@gmail.com>
2026-02-12 06:34:06 +0500

    Address comments
    remove object as parameter
    add docs

--- src/core/format/helpers.test.ts
+++ src/core/format/helpers.test.ts
@@ -10,16 +10,6 @@ describe("format helpers", () => {
   });
 
   describe("map", () => {
-    it("maps values using an object", () => {
-      const map = helpers.map({ a: "b" });
-      expect(map("a")).toBe("b");
-    });
-
-    it("returns the original value if key is not found in object", () => {
-      const map = helpers.map({ a: "b" });
-      expect(map("c")).toBe("c");
-    });
-
     it("maps values using pairs", () => {
       const map = helpers.map("a", "b", "c", "d");
       expect(map("a")).toBe("b");
--- src/core/format/helpers.ts
+++ src/core/format/helpers.ts
@@ -4,21 +4,11 @@ type StringMappingFn = (input: string) => string;
 
 export const lowercase = (): StringMappingFn => (s) => s.toLowerCase();
 
-// This is a mapping function, it can be used in two ways:
-// 1. With an object: map({ a: "b" }) will return a function that maps "a" to "b" and returns the original value for any other input.
-// 2. With pairs of arguments: map("a", "b", "c", "d") will return a function that maps "a" to "b", "c" to "d", and returns the original value for any other input.
 export const map = (
-  objectOrString: Record<string, string> | string,
-  ...otherArgs: string[]
+  ...pairs: string[]
 ): StringMappingFn => {
-  // If the first argument is an object, use it directly.
-  if (typeof objectOrString !== "string") {
-    return (s) => objectOrString[s] || s;
-  }
-
-  // Otherwise, build a mapping object from pairs of arguments once.
   const mapping: Record<string, string> = {};
-  const pairs = [objectOrString, ...otherArgs];
+
   for (let i = 0; i < pairs.length; i += 2) {
     const value = pairs[i + 1];
     if (value !== undefined) {
@@ -29,6 +19,8 @@ export const map = (
   return (s) => mapping[s] ?? s;
 };
 
+map.description = "map('key1', 'value1', 'key2', 'value2', ...)";
+
 export const shellquote = (): StringMappingFn => (s) =>
   `'${s.replace(/'/g, "'\\''")}'`;
 
@@ -37,16 +29,16 @@ export const slugify = (separator = "-"): StringMappingFn =>
 
 export const substring =
   (start: number, end?: number): StringMappingFn =>
-  (s) =>
-    s.substring(start, end);
+    (s) =>
+      s.substring(start, end);
 substring.description = "substring(start-index[, end-index])";
 
 export const trim = (): StringMappingFn => (s) => s.trim();
 
 export const truncate =
   (limit: number): StringMappingFn =>
-  (s) =>
-    s.length > limit ? `${s.substring(0, limit - 1)}…` : s;
+    (s) =>
+      s.length > limit ? `${s.substring(0, limit - 1)}…` : s;
 truncate.description = "truncate(max-length)";
 
 export const uppercase = (): StringMappingFn => (s) => s.toUpperCase();
