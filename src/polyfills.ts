fc0d84e9256bf2868a5c3dad346db80de92e2f83
Max Mulatz <max@bitcrowd.net>
2026-03-03 14:47:04 +0100

    Update JSDOM
    This updates JSDOM, which should unlock a couple of security fixes.
    
    The changes in the types package make our headers polyfill's type
    signature incompatible. I opened a PR on the project to fix this. Up
    until then we can simply use type assertions to keep our code working.
    
    As JSDOM's dependencies are mostly ESM, I also had to update the list of
    packages to ignore when transforming with Babel for Jest. In order to
    keep the list a bit shorter, we only list the parent directory and then
    use (.+/)? to match any number of subdirectories.

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -3,4 +3,9 @@ import { Headers as HeadersPolyfill } from "headers-polyfill";
 // NOTE: With manifest v3, Firefox does not have an up to date implementation of
 // the `Headers` class. This breaks our HTTP client based on `ky`.
 // Therefore we use a polyfill for this:
-globalThis.Headers = HeadersPolyfill;
+//
+// TODO: Remove this type assertion once the `headers-polyfill` package is
+// updated and my PR is merged (https://github.com/mswjs/headers-polyfill/pull/82).
+// TypeScript v5.2+ requires `[Symbol.dispose]` on any `InterableIterator` type
+// and the polyfills iterators don't yet implement this.
+globalThis.Headers = HeadersPolyfill as unknown as typeof Headers;
