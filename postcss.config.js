ebff3f2c99214ae34c223c73b6e360c03fdf7f3f
Max Mulatz <max@bitcrowd.net>
2026-03-05 12:25:52 +0100

    Update purgecss
    There was an API change in purgecss. Updating this dependency unlocks
    some more security fixes for us 🍄.

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,18 @@
-import { purgeCSSPlugin } from "@fullhuman/postcss-purgecss";
+import purgeCSSPlugin from "@fullhuman/postcss-purgecss";
 
 export default {
   plugins: [
-    [
-      purgeCSSPlugin({ content: ["src/**/*.{html,tsx}"] }),
-      {
-        contentFunction: (src) => {
-          if (src.endsWith("src/popup/index.scss")) {
-            return ["src/popup/**/*.{html,ts,tsx}"];
-          }
-          if (src.endsWith("src/options/index.scss")) {
-            return ["src/options/**/*.{html,ts,tsx}"];
-          }
-          throw new Error(`Unexpected CSS entrypoint: ${src}`);
-        },
+    purgeCSSPlugin({
+      contentFunction: (src) => {
+        if (src.endsWith("src/popup/index.scss")) {
+          return ["src/popup/**/*.{html,ts,tsx}"];
+        }
+        if (src.endsWith("src/options/index.scss")) {
+          return ["src/options/**/*.{html,ts,tsx}"];
+        }
+        throw new Error(`Unexpected CSS entrypoint: ${src}`);
       },
-    ],
+    }),
     ["postcss-preset-env", {}],
     ["cssnano", { preset: "default" }],
   ],
